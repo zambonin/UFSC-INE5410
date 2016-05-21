b6212841f44e1ff0c35a4d84163e7c06bff9af48
Gustavo Zambonin <gzmbnn@gmail.com>
2016-05-20 21:14:42 -0300

    fourth set of exercises

---
+++ ine5410_e4-1.c
@@ -0,0 +1,30 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#define SIZE 50000000
+
+void calc_stuff();
+
+int main(int argc, char **argv) {
+
+    calc_stuff();
+
+    return 0;
+
+}
+
+void calc_stuff() {
+
+    double *c = (double *) malloc (sizeof(double) * SIZE);
+
+    # pragma omp parallel for
+    for (int i = 0; i < SIZE; i++) {
+        c[i] = sqrt(i * 32) + sqrt(i * 16 + i * 8) + sqrt(i * 4 + i * 2 + i);
+        c[i] -= sqrt(i * 32 * i * 16 + i * 4 + i * 2 + i);
+        c[i] += pow(i * 32, 8) + pow(i * 16, 12);
+    }
+
+    free(c);
+
+}
